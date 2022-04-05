43cc9191674038ebd5f037c9446e6b310322cf84
MaxenceGui <maxence.guindon@outlook.com>
2022-04-05 11:22:14 -0400

    partial fait

---
+++ partial/header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8" >
+		<title> Magix </title>
+        <link href="css/global.css" rel="stylesheet" />
+        <link href="js/javascript.js"/>
+    </head>
+    <body>
