1ff18dce17e6cca6dc165c1c0b0a160e4dbfe921
MaxenceGui <maxence.guindon@outlook.com>
2022-04-05 13:20:33 -0400

    boutton ajouter dans lobby

--- lobby.php
+++ lobby.php
@@ -7,4 +7,10 @@
 ?>
 <h1> Vous êtes connectés</h1>
 
+<button>Pratique</button>
+<button>Jouez</button>
+<button>Quitter</button>
+<button>Regarder une partie</button>
+<button>Deck</button>
+
 <h6><a href="?logout=true">Déconnexion</a></h6>
