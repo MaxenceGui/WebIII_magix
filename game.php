a9c956e6fb69c276ee85d51c74821ae5a1b605a6
MaxenceGui <maxence.guindon@outlook.com>
2022-04-26 13:11:24 -0400

    commencer le drag

--- game.php
+++ game.php
@@ -5,49 +5,55 @@
 
     require_once("partial/header.php");
 ?>
+<div id="zoneJeu">
+    <div id="zoneTop">
+        <div class="partieDiv">
+            <div class="infoPartie">
+                <button onClick='jouerCoup("SURRENDER")'>Abandonner</button>
+                <button onClick='revenirLobby()'>lobby</button>
+            </div>
+        </div>
+    
+        <div class="michantDiv">
+            <h1 class="texteBienvenu">Game On</h1>
+            <div class="michantClassHero"></div>
+            <div class="infoMichant">
+                <div class="michantNbCarte"></div>
+                <div class="michantHP"></div>
+                <div class="michantMP"></div>
+                <div class="michantLostCount"></div>
+            </div>
+        </div>
 
-<div id="zoneTop">
-    <h1 class="texteBienvenu">Game On</h1>
+        <div class="partieDiv">
+            <div class="infoPartie">
+                <div class="remaingTurnTime"></div>
+                <button onClick='jouerCoup("END_TURN")'>Fin du tour</button>
+            </div>
+        </div>
+    </div>
 
-    <div class="michantDiv">
-        <div class="michantClassHero"></div>
-        <div class="infoMichant">
-            <div class="michantNbCarte"></div>
-            <div class="michantHP"></div>
-            <div class="michantMP"></div>
-            <div class="michantLostCount"></div>
+    <div class="michantBoard"></div>
+
+
+    <div class="joueurBoard"></div>
+
+    <div id="zoneJoueur">
+        <div class="joueurDiv">
+            <div class="infoJoueur">
+                <div class="joueurClassHero"></div>
+                <div class="joueurHP"></div>
+                <div class="joueurMP"></div>
+                <div class="tourJoueur"></div>
+            </div>
+        </div>
+
+        <div class="carteEnMain"></div>
+
+        <div class="joueurDiv">
+            <div class="remaingCardsCount"></div>
+            <div class="talent"></div>
+            <button onClick='jouerCoup("HERO_POWER")'>Pouvoir</button>
         </div>
     </div>
 </div>
-
-<div class="michantBoard"></div>
-
-
-
-
-<div class="joueurDiv">
-    <div class="infoJoueur">
-        <div class="joueurClassHero"></div>
-        <div class="joueurHP"></div>
-        <div class="joueurMP"></div>
-    </div>
-    <div class="joueurBoard"></div>
-</div>
-
-<div class="partieDiv">
-    <div class="infoPartie">
-        <div class="remaingCardsCount"></div>
-        <div class="remaingTurnTime"></div>
-        <div class="talent"></div>
-    </div>
-</div>
-
-<div class="carteEnMain">
-</div>
-
-<div class="tourJoueur"></div>
-
-<button onClick='jouerCoup("SURRENDER")'>Abandonner</button>
-<button onClick='jouerCoup("HERO_POWER")'>Pouvoir</button>
-<button onClick='jouerCoup("END_TURN")'>Fin du tour</button>
-<button onClick='revenirLobby()'>lobby</button>
--- js/javascript.js
+++ js/javascript.js
@@ -1,8 +1,25 @@
 let carteEnMain = [];
 let carteMechant = [];
+let carteEnJeu = [];
+let draggable = false;
+
 window.addEventListener("load",() =>{
     carteEnMain = [];
     carteMechant = [];
+    carteEnJeu = [];
+
+
+    // Pour le toucher du drag
+    surfaceJoueur = document.querySelector(".joueurBoard");
+    surfaceJoueur.addEventListener("touchstart", dragStart, false);
+    surfaceJoueur.addEventListener("touchend", dragEnd, false);
+    surfaceJoueur.addEventListener("touchmove", drag, false);
+
+    // Pour la souris du drag
+    surfaceJoueur.addEventListener("mousedown", dragStart, false);
+    surfaceJoueur.addEventListener("mouseup", dragEnd, false);
+    surfaceJoueur.addEventListener("mousemove", drag, false);
+
     let lieu = location.href
     if (lieu == "http://localhost/WebIII_magix/game.php"){
         setTimeout(gameState, 1000); // Appel initial (attendre 1 seconde)
@@ -104,7 +121,6 @@ const gameState = () =>{
                     // frame de la carte             
                     let carteStruct = document.createElement("div");
                     carteStruct.classList.add("michantCarte");
-                    //carteStruct.innerHTML = "Carte : " + carte + " HP : " + result.hand[carte].hp + " Coût : "  + result.hand[carte].cost + " ATK : " + result.hand[carte].atk + "\n" + " Description : " + result.hand[carte].mechanics;
                     
                     // boite pour la vie
                     let carteVie = document.createElement("div");
@@ -135,13 +151,49 @@ const gameState = () =>{
                 } 
             }
     
-             // information du joueur
-             document.querySelector(".joueurClassHero").innerHTML = "classe du joueur : " + result.heroClass;
-             document.querySelector(".joueurHP").innerHTML = "HP du joueur : " + result.hp;
-             document.querySelector(".joueurMP").innerHTML = "MP du joueur : " + result.mp;
+            // information du joueur
+            document.querySelector(".joueurClassHero").innerHTML = "classe du joueur : " + result.heroClass;
+            document.querySelector(".joueurHP").innerHTML = "HP du joueur : " + result.hp;
+            document.querySelector(".joueurMP").innerHTML = "MP du joueur : " + result.mp;
             
-             // Ses cartes en jeu
-             document.querySelector(".joueurBoard").innerHTML = "Jeu du joueur : " + result.board;
+            // Ses cartes en jeu
+            for (const carte in result.board) {
+                if (carteEnJeu[carte] != result.board[carte].id){
+                    
+                    carteEnJeu[carte] = (result.board[carte].id);   
+                    
+                    // frame de la carte             
+                    let carteStruct = document.createElement("div");
+                    carteStruct.classList.add("joueurCarte");
+
+                    // boite pour la vie
+                    let carteVie = document.createElement("div");
+                    carteVie.classList.add("carteInfoVie");
+                    carteStruct.append(carteVie);
+                    carteVie.innerHTML = " HP : " + result.board[carte].hp;
+
+                    // // boite pour le coût
+                    carteCout = document.createElement("div");
+                    carteCout.classList.add("carteInfoCout");
+                    carteStruct.append(carteCout);
+                    carteCout.innerHTML = " Coût : "  + result.board[carte].cost;
+
+                    // // boite pour l'attaque
+                    carteAtk = document.createElement("div");
+                    carteAtk.classList.add("carteInfoAtk");
+                    carteAtk.innerHTML = " ATK : " + result.board[carte].atk;
+                    carteStruct.append(carteAtk);
+
+                    // // Description
+                    carteInfo = document.createElement("div");
+                    carteInfo.classList.add("carteInfoHab");
+                    carteInfo.innerHTML = " Description \n" + result.hand[carte].mechanics;
+                    carteStruct.append(carteInfo);
+
+                    document.querySelector(".joueurBoard").append(carteStruct);
+
+                } 
+            }
             
              // Ses cartes en main
             for (const carte in result.hand) {
@@ -152,7 +204,6 @@ const gameState = () =>{
                     // frame de la carte             
                     let carteStruct = document.createElement("div");
                     carteStruct.classList.add("joueurCarte");
-                    //carteStruct.innerHTML = "Carte : " + carte + " HP : " + result.hand[carte].hp + " Coût : "  + result.hand[carte].cost + " ATK : " + result.hand[carte].atk + "\n" + " Description : " + result.hand[carte].mechanics;
                     
                     // boite pour la vie
                     let carteVie = document.createElement("div");
@@ -182,16 +233,53 @@ const gameState = () =>{
 
                 } 
             }
-             
-
-             // info sur la partie
-             document.querySelector(".remaingCardsCount").innerHTML = "Carte du joueur : " + result.remainingCardsCount;
-             document.querySelector(".remaingTurnTime").innerHTML = "Temps du tour : " + result.remainingTurnTime;
-             document.querySelector(".talent").innerHTML = "Talent : " + result.talent;
+            
+            // info sur la partie
+            document.querySelector(".remaingCardsCount").innerHTML = "Carte du joueur : " + result.remainingCardsCount;
+            document.querySelector(".remaingTurnTime").innerHTML = "Temps du tour : " + result.remainingTurnTime;
+            document.querySelector(".talent").innerHTML = "Talent : " + result.talent;
     
-             // tour du joueur?
-             document.querySelector(".tourJoueur").innerHTML = "Tour du joueur? : " + result.yourTurn;
+            // tour du joueur?
+            document.querySelector(".tourJoueur").innerHTML = "Tour du joueur? : " + result.yourTurn;
         }     
         setTimeout(gameState, 1000); // en plaçant le setTimeout ici on évite de faire des appels en recevant le résultat
     })
+}
+
+// Source : https://www.kirupa.com/html5/drag.htm
+
+const dragStart = (e) =>{
+
+    let startX = 0;
+    let startY = 0;
+
+    if (e.type == "touchstart"){
+        startX = e.touches[0].clientX - 0;
+        startY = e.touches[0].clientY - 0;
+    }
+    else{
+        startX = e.clientX - 0;
+        startY = e.clientY - 0;
+    }
+
+    if (e.target == document.querySelector(".joueurCarte")){
+        draggable = true;
+    }
+}
+
+const drag = (e) =>{
+
+    let currentX = 0;
+    let currentY = 0;
+
+    if (draggable){
+        
+        e.preventDefault();
+
+
+        if (e.type == "touchmove"){
+            currentX = e.touches[0].clientX - startX;
+            currentY = e.touches[0].clientY - startY;
+        }
+    }
 }
