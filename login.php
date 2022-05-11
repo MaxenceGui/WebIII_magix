846e1770b717759613f4223443d0023b809fb08b
MaxenceGui <96267006+MaxenceGui@users.noreply.github.com>
2022-05-10 20:51:45 -0400

    Avancé page login
    avancé page login

--- login.php
+++ login.php
@@ -6,25 +6,29 @@
     $data = $action->execute();
 ?>
 
-<h1> Connection </h1>
+<div id="zoneAccueil">
+<div class="character"></div>
+    <div id="accueil">
+        <h1> Connexion </h1>
+        <form class="formulaire" action="login.php" method="post">
+            <div class="usager_label">
+                <label for="username">Nom d'usager</label>
+            </div>
+            <div class="usager_input">
+                <input type="text" name="username" id="username" />
+            </div>
 
-<form action="login.php" method="post">
-    <div class="usager_label">
-        <label for="username">Nom d'usager</label>
-    </div>
-    <div class="usager_input">
-        <input type="text" name="username" id="username" />
-    </div>
+            <div class="mdp_label">
+                <label for="password">Mot de passe</label>
+            </div>
+            <div class="usager_input">
+                <input type="password" name="password" id="pwd" />
+            </div>
 
-    <div class="mdp_label">
-        <label for="password">Mot de passe</label>
+            <div class="button_div">
+                <button type="submit">Connexion</button>
+            </div>
+        </form>
     </div>
-    <div class="usager_input">
-        <input type="password" name="password" id="pwd" />
-    </div>
-
-    <div class="button_div">
-        <button type="submit">Connexion</button>
-    </div>
-    
-</form>
+</div>
+<div class="arcade"></div>
